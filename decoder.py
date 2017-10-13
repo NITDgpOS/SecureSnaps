8aa898eb5260f89715fb91be6afc836216834457
Bishakh Ghosh <ghoshbishakh@gmail.com>
2017-10-13 16:33:13 +0530

    Add requirements.txt and fix PEP8 issues

--- decoder.py
+++ decoder.py
@@ -1,28 +1,27 @@
 from PIL import Image
-import math
 from keygen import *
 from utils import *
 
-image_path= 'Enc/painting_en.png'
+image_path = 'Enc/painting_en.png'
 im = Image.open(image_path, "r")
-arr = im.load() #pixel data stored in this 2D array
-(W,H) = im.size
+arr = im.load()  # pixel data stored in this 2D array
+(W, H) = im.size
 degree = int(input())
-print(W,H)
+print(W, H)
 
-KEY = generate_tuples(H,W)
+KEY = generate_tuples(H, W)
 
 for i in range(4):
-	# ith Wave
-	first= cascade(KEY[3-i][0:2],degree, W, H)
-	second= cascade(KEY[3-i][2:],degree, W, H)
-	automate_swap_dec(first,second,degree+1,im,arr)
+    # ith Wave
+    first = cascade(KEY[3 - i][0:2], degree, W, H)
+    second = cascade(KEY[3 - i][2:], degree, W, H)
+    automate_swap_dec(first, second, degree + 1, im, arr)
 
-# im.show() #To display the image im	
-im.save("Dec/"+image_path[4:-8]+"_dec"+".png")
+# im.show() #To display the image im
+im.save("Dec/" + image_path[4:-8] + "_dec" + ".png")
 im2 = Image.open('Temp/painting.png', "r")
 
-#To calculate efficiency of the algorithm
-arr2= im2.load()
+# To calculate efficiency of the algorithm
+arr2 = im2.load()
 efficiency(arr2, arr, W, H)
 im.show()
